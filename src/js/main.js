0ceb25e36a683b3d7c0786984a82abd08b13b02e
TheSpaceHuman <kolodyazhnyartem@gmail.com>
2018-11-30 17:14:33 +0300

    index 40%

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+//User js
+//Slider
+$('.goods-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.index-slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1
+  // centerMode: true,
+  // variableWidth: true
+});
