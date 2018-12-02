6cb98f4e57608a7d39da1375e65291e959d7af1b
TheSpaceHuman <kolodyazhnyartem@gmail.com>
2018-12-03 00:14:25 +0300

    index 60%

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,14 @@ $('.goods-slider').slick({
   dots: true,
   infinite: true,
   speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToShow: 5,
+  slidesToScroll: 5,
   responsive: [
     {
       breakpoint: 1200,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         infinite: true,
         dots: true
       }
